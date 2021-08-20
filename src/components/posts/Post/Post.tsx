24c9ed9bb7c90c8437097fb13edbaf5cb4ddb8d7
feralwater <lineviktoriya@gmail.com>
2021-08-20 16:36:59 +0300

    Add: post styles

--- src/components/posts/Post/Post.tsx
+++ src/components/posts/Post/Post.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import style from "./Post.module.scss"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faThumbsUp, faThumbsDown} from '@fortawesome/free-solid-svg-icons'
+
+
+const likeButtonIcon = <FontAwesomeIcon icon={faThumbsUp}/>
+const disLikeButtonIcon = <FontAwesomeIcon icon={faThumbsDown}/>
 
 const Post = () => {
     return (
         <>
-            <div>post</div>
+            <div className={style.user}>
+                <div>
+                    <img className={style.user__image} src="./img/avatar.png" alt="avatar"/>
+                </div>
+                <div className={style.user__post}>
+                    <div className={style.user__name}>Johnson Smith</div>
+                    <div className={style.post__body}>post body</div>
+                </div>
+            </div>
+            <div className={style.buttons}>
+                <button>
+                    <i>{likeButtonIcon}</i>
+                    <span>123</span></button>
+                <button>
+                    <i>{disLikeButtonIcon}</i>
+                    <span>1</span></button>
+            </div>
         </>
     );
 };
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import style from "./Posts.module.scss";
 import Post from "./Post/Post";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlusCircle} from '@fortawesome/free-solid-svg-icons'
+
+
+const addButtonIcon = <FontAwesomeIcon icon={faPlusCircle}/>
 
 const Posts = () => {
     return (
         <main className={style.main}>
-            <textarea ></textarea>
-            <button>add post</button>
+            <div className={style.form}>
+                <textarea className={style.form__textarea} placeholder={"Share your mind"}>
+                </textarea>
+                <button className={style.form__button}>
+                    <i>{addButtonIcon}</i>
+                    Add
+                </button>
+            </div>
+            <Post/>
             <Post/>
         </main>
     );
